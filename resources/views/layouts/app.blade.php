b6dd70b7aa80a9c6b02a660d987e6b9c1e8db780
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-05-23 13:51:04 +0800

    set poppins as default font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,9 +24,6 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet" />
 
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css') }}" />
 
