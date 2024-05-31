6919caccf9a4d668d1073da262a58c88b9b9128f
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-05-31 22:58:16 +0800

    select2 and sweetalert2 local dependency

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,90 +1,99 @@
 @extends('layouts.app')
 
 @section('body')
-<div class="layout-wrapper layout-content-navbar">
-    <div class="layout-container">
-      <!-- Menu -->
-      @include('components.sidebar')
-      <!-- / Menu -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+            @include('components.sidebar')
+            <!-- / Menu -->
 
-      <!-- Layout container -->
-      <div class="layout-page">
-        <!-- Navbar -->
-        @include('components.navbar')
-        <!-- / Navbar -->
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+                @include('components.navbar')
+                <!-- / Navbar -->
 
-        <!-- Content wrapper -->
-        <div class="content-wrapper">
-          <!-- Content -->
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
 
-          <div class="container-xxl flex-grow-1 container-p-y">
-              @yield('content')
-          </div>
-          <!-- / Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        @yield('content')
+                    </div>
+                    <!-- / Content -->
 
-          <!-- Footer -->
-          @include('components.footer')
-          <!-- / Footer -->
+                    <!-- Footer -->
+                    @include('components.footer')
+                    <!-- / Footer -->
 
-          <div class="content-backdrop fade"></div>
+                    <div class="content-backdrop fade"></div>
+                </div>
+                <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
         </div>
-        <!-- Content wrapper -->
-      </div>
-      <!-- / Layout page -->
-    </div>
 
-    <!-- Overlay -->
-    <div class="layout-overlay layout-menu-toggle"></div>
-</div>
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
 @endsection
 
-@push('script')
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script>
-    const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-            toast.onmouseenter = Swal.stopTimer;
-            toast.onmouseleave = Swal.resumeTimer;
-        }
-    });
+@push('style')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2-bootstrap-5-theme.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.min.css') }}"/>
+@endpush
 
-    function confirmSubmit(e, form) {
-        e.preventDefault();
-        Swal.fire({
-            title: "{{ __('label.warning') }}",
-            text: "{{ __('label.are_you_sure') }}",
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonText: "{{ __('label.yes_i_am') }}",
-            cancelButtonText: "{{ __('label.cancel') }}",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                form.submit();
+@push('script')
+    <script src="{{ asset('assets/vendor/libs/select2/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.all.min.js') }}"></script>
+    <script>
+        $('.select2').select2({theme: 'bootstrap-5'});
+
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "bottom-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
             }
         });
 
-        return false;
-    }
-</script>
+        function confirmSubmit(e, form) {
+            e.preventDefault();
+            Swal.fire({
+                title: "{{ __('label.warning') }}",
+                text: "{{ __('label.are_you_sure') }}",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: "{{ __('label.yes_i_am') }}",
+                cancelButtonText: "{{ __('label.cancel') }}",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.submit();
+                }
+            });
 
-@session('notification')
-<script>
-    Toast.fire({
-        icon: "{{ $value['icon'] }}",
-        title: "{{ $value['title'] ?? '' }}",
-        text: "{{ $value['message'] ?? '' }}",
-    });
-</script>
-@endsession
+            return false;
+        }
+    </script>
 
-<script>
-    document.getElementById('logout-menu-button-on-nav').addEventListener('click', function() {
-        document.getElementById('logout-menu-form-on-nav').submit();
-    });
-</script>
+    @session('notification')
+    <script>
+        Toast.fire({
+            icon: "{{ $value['icon'] }}",
+            title: "{{ $value['title'] ?? '' }}",
+            text: "{{ $value['message'] ?? '' }}",
+        });
+    </script>
+    @endsession
+
+    <script>
+        document.getElementById('logout-menu-button-on-nav').addEventListener('click', function () {
+            document.getElementById('logout-menu-form-on-nav').submit();
+        });
+    </script>
 @endpush
